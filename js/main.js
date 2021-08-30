7033eea537254fc03b249933fe908f4a606a3b45
wkazxf <84431962+wkazxf@users.noreply.github.com>
2021-08-30 18:04:44 +0900

    2021.08.30
    모든 확진자 case push 및 최신화

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 //아주대 주요 건물 좌표 정보 배열
 //placeCoordinates[index].xpos  //placeCoordinates[index].ypos
-//0 성호관  1 아주대    2 도서관    3 체육관    4 남제관    5 용지관    6 화홍관    7 광교관    8 국제학사
-//9 팔달관  10 동관    11 다산관   12 산학원
 const placeCoordinates = [{
     xpos: "37.282906900486445",
     ypos: "127.0450830338577",
@@ -77,6 +75,36 @@ const placeCoordinates = [{
     ypos: "127.0457899948619",
     place: "산학원"
     //산학원, placeCoordinates[12]
+},
+{
+    xpos: "37.28346603769236",
+    ypos: "127.04376965752795",
+    place: "원천관"
+    //원천관, placeCoordinates[13]
+},
+{
+    xpos: "37.28223241318523",
+    ypos: "127.04753804484503",
+    place: "연암관"
+    //연암관, placeCoordinates[14]
+},
+{
+    xpos: "37.283363802252445 ",
+    ypos: "127.04605872055546 ",
+    place: "신학생회관"
+    //신학생회관, placeCoordinates[15]
+},
+{
+    xpos: "37.282316233282216",
+    ypos: "127.04634280893518",
+    place: "율곡관"
+    //율곡관, placeCoordinates[16]
+},
+{
+    xpos: "37.27922652346397",
+    ypos: "127.04410265898133 ",
+    place: "캠퍼스플라자"
+    //캠퍼스플라자, placeCoordinates[17]
 }
 ];
 
@@ -94,11 +122,36 @@ function coronicList_push(year, month, date, place, identity) {
 }
 
 //확진자 리스트에 아래의 확진자들을 추가
-coronicList_push(2021, 8, 29, [{ place: 2, hour: 17, min: 30 }, { place: 11, hour: 15, min: 50 }], "학생");
-coronicList_push(2021, 8, 29, [{ place: 3, hour: 17, min: 30 }, { place: 5, hour: 15, min: 50 }], "교직원");
+coronicList_push(2021, 6, 7, [{ place: 13, s_hour: 10, s_min: 30, e_hour: 12, e_min: 00 }, { place: 2, s_hour: 12, s_min: 00, e_hour: 12, e_min: 55 }], "재학생");
+coronicList_push(2021, 6, 16, [{ place: 11, s_hour: 15, s_min: 00, e_hour: 16, e_min: 20 }], "재학생");
+coronicList_push(2021, 6, 17, [{ place: 11, s_hour: 12, s_min: 00, e_hour: 16, e_min: 15 }], "재학생");
+coronicList_push(2021, 6, 29, [{ place: 14, s_hour: 7, s_min: 00, e_hour: 17, e_min: 00 }, { place: 15, s_hour: 11, s_min: 00, e_hour: 11, e_min: 05 }], "대학원생");
+coronicList_push(2021, 6, 30, [{ place: 14, s_hour: 7, s_min: 00, e_hour: 16, e_min: 30 }], "대학원생");
+coronicList_push(2021, 7, 1, [{ place: 14, s_hour: 7, s_min: 00, e_hour: 13, e_min: 30 }], "대학원생");
+coronicList_push(2021, 7, 2, [{ place: 14, s_hour: 7, s_min: 00, e_hour: 17, e_min: 00 }], "대학원생");
+coronicList_push(2021, 7, 1, [{ place: 6, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "대학원생");
+coronicList_push(2021, 7, 2, [{ place: 6, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "대학원생");
+coronicList_push(2021, 7, 3, [{ place: 6, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "대학원생");
+coronicList_push(2021, 7, 8, [{ place: 1 }], "대학원생");
+coronicList_push(2021, 7, 12, [{ place: 1 }], "교수A 및 교직원");
+coronicList_push(2021, 7, 26, [{ place: 16, s_hour: 9, s_min: 00, e_hour: 10, e_min: 00 }, { place: 9, s_hour: 10, s_min: 00, e_hour: 14, e_min: 00 }], "외부 공사 근로자");
+coronicList_push(2021, 7, 26, [{ place: 2, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "자료실 면학장학생");
+coronicList_push(2021, 7, 27, [{ place: 2, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "자료실 면학장학생");
+coronicList_push(2021, 7, 28, [{ place: 2, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "자료실 면학장학생");
+coronicList_push(2021, 7, 29, [{ place: 2, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "자료실 면학장학생");
+coronicList_push(2021, 8, 1, [{ place: 1 }], "재학생 2명");
+coronicList_push(2021, 7, 30, [{ place: 17, s_hour: 9, s_min: 20, e_hour: 17, e_min: 30 }], "용지관 거주 재학생");
+coronicList_push(2021, 8, 4, [{ place: 5, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "용지관 거주 재학생");
+coronicList_push(2021, 8, 2, [{ place: 16, s_hour: 8, s_min: 20, e_hour: 16, e_min: 00 }, { place: 11, s_hour: 16, s_min: 00, e_hour: 16, e_min: 15 }], "교직원");
+coronicList_push(2021, 8, 3, [{ place: 16, s_hour: 8, s_min: 20, e_hour: 11, e_min: 00 }], "교직원");
+coronicList_push(2021, 8, 6, [{ place: 8, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "국제학사 거주 재학생");
+coronicList_push(2021, 8, 6, [{ place: 5, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "용지관 거주 재학생");
+coronicList_push(2021, 8, 25, [{ place: 1 }], "재학생A, 대학원생A, 교직원");
+coronicList_push(2021, 8, 30, [{ place: 8, s_hour: 0, s_min: 00, e_hour: 24, e_min: 00 }], "국제학사 거주 재학생");
 
 //0 성호관  1 아주대    2 도서관    3 체육관    4 남제관    5 용지관    6 화홍관    7 광교관    8 국제학사
-//9 팔달관  10 동관    11 다산관   12 산학원
+//9 팔달관  10 동관    11 다산관   12 산학원   13 원천관   14 연암관   15 신학     16 율곡관   17 캠퍼스플라자
+
 
 //-------------------------------------------------------------------------------------------------------------
 
@@ -177,10 +230,14 @@ function makeMarker(coronicReturnList) {
             // 마커가 지도 위에 보이도록 표시
             marker.setMap(map);
             //인포윈도우 내용
-            var iwContent = '<div style="padding:5px;font-size:0.8rem;">' + coronicReturnList[i].year + '년 ' + coronicReturnList[i].month +
-                '월' + coronicReturnList[i].date + '일 <br>' + coronicReturnList[i].place[j].hour + '시 ' + coronicReturnList[i].place[j].min + '분 ' +
-                placeCoordinates[markerIndex].place + ' ' + coronicReturnList[i].identity + String.fromCharCode(asciiNum) + '</div>',
-                iwPosition = new kakao.maps.LatLng(placeCoordinates[0].xpos, placeCoordinates[0].ypos); //인포윈도우 표시 위치
+            if (markerIndex == 1) { var iwContent = '<div style="padding:5px;font-size:0.8rem;">' + coronicReturnList[i].identity + String.fromCharCode(asciiNum) + '<br>교내 동선 해당없음 </div>' }
+            else {
+                var iwContent = '<div style="padding:5px;font-size:0.8rem;">' + coronicReturnList[i].year + '년 ' + coronicReturnList[i].month +
+                    '월' + coronicReturnList[i].date + '일 <br>' + coronicReturnList[i].place[j].s_hour + '시 ' + coronicReturnList[i].place[j].s_min + '분 ~ '
+                    + coronicReturnList[i].place[j].e_hour + '시 ' + coronicReturnList[i].place[j].e_min + '분<br>' +
+                    placeCoordinates[markerIndex].place + ' ' + coronicReturnList[i].identity + String.fromCharCode(asciiNum) + '</div>',
+                    iwPosition = new kakao.maps.LatLng(placeCoordinates[0].xpos, placeCoordinates[0].ypos); //인포윈도우 표시 위치
+            }
             // 인포윈도우를 생성합니다
             var infowindow = new kakao.maps.InfoWindow({
                 position: iwPosition,
