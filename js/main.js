5fce1809c91bdcdeb173944da1e4205fb36f1019
Donghwan Lee <84431962+wkazxf@users.noreply.github.com>
2021-12-31 00:42:30 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -187,6 +187,7 @@ coronicList_push(2021, 12, 21, [{ place: 1 }], "재학생");
 coronicList_push(2021, 12, 16, [{ place: 19, s_hour: 08, s_min: 30, e_hour: 16, e_min: 00 }], "재학생");
 coronicList_push(2021, 12, 22, [{ place: 11, s_hour: 09, s_min: 00, e_hour: 10, e_min: 30 }, { place: 2, s_hour: 10, s_min: 30, e_hour: 12, e_min: 00 },
 { place: 11, s_hour: 12, s_min: 00, e_hour: 13, e_min: 00 }], "재학생");
+coronicList_push(2021, 12, 24, [{ place: 15, s_hour: 14, s_min: 00, e_hour: 15, e_min: 00 }], "대학원생");
 //0 성호관  1 아주대    2 도서관    3 체육관    4 남제관    5 용지관    6 화홍관    7 광교관    8 국제학사
 //9 팔달관  10 동관    11 다산관   12 산학원   13 원천관   14 연암관   15 신학     16 율곡관   17 캠퍼스플라자
 //18 서관   19 송제관
